90b18de8a8e1de1c278fbbc8cc3b1fc3d2adacd2
Adrian Peter Krone <adrian.krone@physik.uni-kassel.de>
2025-11-05 10:58:48 +0100

    Override awful python devicon color

--- init.lua
+++ init.lua
@@ -379,7 +379,19 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      {
+        'nvim-tree/nvim-web-devicons',
+        enabled = vim.g.have_nerd_font,
+        opts = {
+          override_by_extension = {
+            ['py'] = {
+              icon = '',
+              color = '#f9e2af',
+              name = 'Python',
+            },
+          },
+        },
+      },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
